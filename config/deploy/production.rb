3465f82701044d22a0ba607f7ca0ab931b14d5a3
shubham jain <shubhamjain.2303@gmail.com>
2020-01-29 15:15:06 +0530

    captrno setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,27 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '13.232.18.189', user: 'deploy'
+
+role :app, %w{deploy@13.232.18.189}
+role :web, %w{deploy@13.232.18.189}
+role :db,  %w{deploy@13.232.18.189}
+
+set :stage, :production
+set :rails_env, :production
+
+set :deploy_user, "deploy"
+
+server '13.232.18.189', user: 'deploy', roles: %w{web app db}, primary: true
+
+# set :ssh_options, {
+# 	ubuntu: 'ubuntu',
+# 	forward_agent: false,
+# 	auth_methods: %w(password),
+# 	password: 'password'
+# }
+
+set :ssh_options, {
+    keys: %w(/Users/shubhamjain/Downloads/shubham_temp2.pem),
+    forward_agent: false,
+    auth_methods: %w(publickey)
+}
